191270b884f57e9f1eabf90d1f5f5fc652429401
nikolveloz922-hash <nikolveloz922@gmail.com>
2026-09-24 16:22:21 -0400

    Update seed.sql

--- backend/seed.sql
+++ backend/seed.sql
@@ -1,4 +1,2 @@
-backend/seed.sql
-INSERT OR IGNORE INTO hotels(id,name,category,state,city,zone,normal_price,findbed_price,deposit,rooms,photo,gallery_json,active)
-VALUES('hotel-carmesi-royal','Hotel Carmesí Royal','Hotel','Carabobo','Valencia','El Viñedo',80,70,10,4,'hotel1_piscina.jpg',
-'["hotel1_piscina.jpg","hotel1_habitacion.jpg","hotel1_bano.jpg","hotel1_restaurante.jpg"]',1);
+
+INSERT OR IGNORE INTO hotels(id,name,category,level,state,city,zone,normal_price,findbed_price,deposit,rooms,photo,gallery_json,description,active) VALUESINSERT OR IGNORE INTO hotels(id,name,category,level,state,city,zone,normal_price,findbed_price,deposit,rooms,photo,gallery_json,description,active) VALUES
